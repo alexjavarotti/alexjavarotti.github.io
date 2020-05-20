6bec71e30ebab5d6ad7293c26df3dc8c55c65f74
alex.javarotti.developer@gmail.com <alex.javarotti.developer@gmail.com>
2020-05-20 17:04:48 -0600

    DateToString included as common function

--- library/data.normalization.js
+++ library/data.normalization.js
@@ -118,3 +118,10 @@ function addDays(date, days) {
     return result;
 }
 
+function dateToString(date) {
+    const ano = date.getFullYear();
+    const mes = (`00${date.getMonth() + 1}`).slice(-2);
+    const dia = (`00${date.getDate()}`).slice(-2);
+    return `${dia}/${mes}/${ano}`;
+}
+

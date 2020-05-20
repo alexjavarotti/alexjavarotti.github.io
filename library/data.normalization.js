c34230396eb7e7506155f1b33826c7d2264f8b41
alex.javarotti.developer@gmail.com <alex.javarotti.developer@gmail.com>
2020-05-20 16:05:34 -0600

    Remove header function

--- library/data.normalization.js
+++ library/data.normalization.js
@@ -1,24 +1,3 @@
-async function requestData() {
-    alagoas_casos_acumulados = 'https://raw.githubusercontent.com/C4NESub9/googleData/master/dataAl/CAAiL_Al.csv'
-    bahia_casos_acumulados = 'https://raw.githubusercontent.com/C4NESub9/googleData/master/dataAl/CABiA_Al.csv'
-    ceara_casos_acumulados = 'https://raw.githubusercontent.com/C4NESub9/googleData/master/dataAl/CACiE_Al.csv'
-    maranhao_casos_acumulados = 'https://raw.githubusercontent.com/C4NESub9/googleData/master/dataAl/CAMiA_Al.csv'
-
-
-    try {
-        let [alagoas, bahia, ceara, maranhao] = await Promise.all([
-            fetch(alagoas_casos_acumulados).then(response => response.text().then(text => text)),
-            fetch(bahia_casos_acumulados).then(response => response.text().then(text => text)),
-            fetch(ceara_casos_acumulados).then(response => response.text().then(text => text)),
-            fetch(maranhao_casos_acumulados).then(response => response.text().then(text => text))
-        ]);
-        return { serie1: alagoas, serie2: bahia, serie3: ceara, serie4: maranhao }
-    }
-    catch (err) {
-        console.log(err);
-    };
-}
-
 async function requestData4(serie1, serie2, serie3, serie4) {
     try {
         let [data_serie1, data_serie2, data_serie3, data_serie4] = await Promise.all([
@@ -54,4 +33,7 @@ async function requestData9(serie1, serie2, serie3, serie4, serie5, serie6, seri
     };
 }
 
+function removeHeaders(list) {
+    return list.splice(0, 1);
+}
 
